042b3ae983c9d1522cc4e01246a7bd89153d11c1
lizhiqiangcs <lizhiqiangcs@outlook.com>
2016-05-12 14:18:09 +0800

    start

---
+++ BuptRoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuptRoad
+//
+//  Created by 李志强 on 16/5/12.
+//  Copyright © 2016年 LaFleur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BuptRoad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BuptRoad
+//
+//  Created by 李志强 on 16/5/12.
+//  Copyright © 2016年 LaFleur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
